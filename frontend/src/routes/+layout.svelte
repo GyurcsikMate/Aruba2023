36be465177be0677cbb3288d2acbba2b4a213ac7
GyurcsikMate <ovivankenobi3@gmail.com>
2023-10-28 22:11:12 +0200

    sok javitas

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -10,43 +10,25 @@
 </script>
 
 <!-- App Shell -->
-<AppShell slotSidebarLeft="w-52 bg-surface-500/10 p-4">
+<AppShell slotSidebarLeft="w-52 bg-surface-400 p-4" slotHeader="bg-surface-300">
 	<svelte:fragment slot="header">
 		<!-- App Bar -->
-		<AppBar>
+		<AppBar background="bg-surface-300">
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<strong class="text-xl uppercase"><img class="w-60" src="/LOGO.png" /></strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Marketplace
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Dashboard
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
+				<a class="btn btn-sm variant-ghost-surface" href="/"> Home </a>
+				<a class="btn btn-sm variant-ghost-surface" href="/marketplace"> Marketplace </a>
+				<a class="btn btn-sm variant-ghost-surface" href="/dashboard"> Dashboard </a>
+				<a class="btn btn-sm variant-ghost-surface" href="https://github.com/skeletonlabs/skeleton">
 					Q&A
 				</a>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft">
-		<Navigation/>
+		<Navigation />
 	</svelte:fragment>
 	<!-- Page Route Content -->
 	<div class="container p-10 mx-auto">
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -4,34 +4,44 @@
 	<div class="space-y-10 text-center flex flex-col items-center">
 		<h2 class="h2">Cloud-9</h2>
 		<div class="wrap">
-			<a href="#" class="tailwind.config.ts">
-			  <!-- Tile Content Goes Here -->
-			  <span>Text</span>
+			<a href="/" class="tailwind.config.ts">
+				<!-- Tile Content Goes Here -->
+				<span>Text</span>
 			</a>
-		  
-			<a href="#" class="tile tile-medium">
-			  <!-- Tile Content Goes Here -->
-			  <span>Text</span>
+
+			<a href="/" class="tile tile-medium">
+				<!-- Tile Content Goes Here -->
+				<span>Text</span>
 			</a>
-		  
-			<a href="#" class="tile tile-large">
-			  <!-- Tile Content Goes Here -->
-			  <span>Text</span>
-			</a>
-		  </div>
-		<!-- / -->
-		<div class="flex justify-center space-x-2">
-			<a
-				class="btn variant-filled"
-				href="https://skeleton.dev/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				Launch Documentation
+
+			<a href="/" class="tile tile-large">
+				<!-- Tile Content Goes Here -->
+				<span>Text</span>
 			</a>
 		</div>
-		
+		<!-- / -->
+		<div class="flex justify-center space-x-2">
+			<button
+				class="btn variant-filled"
+				on:click={async () => {
+					let data = await fetch('http://0.0.0.0:8000/list');
+
+					console.log(data);
+				}}
+			>
+				varazslat
+			</button>
+			<button
+				class="btn variant-filled"
+				on:click={async () => {
+					fetch('http://0.0.0.0:8000/list')
+						.then((response) => response.text())
+						.then((result) => console.log(result))
+						.catch((error) => console.log('error', error));
+				}}
+			>
+				varazslat2
+			</button>
+		</div>
 	</div>
 </div>
-
-
