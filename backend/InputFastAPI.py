951418c3f059fc8b9fbca60a33db9b48d3826507
kerteszlevi <kerteszlevi@gmail.com>
2023-10-28 16:06:54 +0200

    cors

--- backend/InputFastAPI.py
+++ backend/InputFastAPI.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 class InputFastAPI:
+    _origins = [
+        "*"
+    ]
+
     def __init__(self, kubernetes):
         self.app = FastAPI()
         self._kubernetes = kubernetes
+        self.app.add_middleware(
+            CORSMiddleware,
+            allow_origins=InputFastAPI._origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
 
         @self.app.get("/list")
         async def get_pods():
